429b4890854d588e0184e8b31a22cf63a5503c2c
BishalKrShaw <bishalshaw.mfis@gmail.com>
2024-05-06 02:10:00 +0530

    Menu Display some tweaks

--- JavaScript/menuDisplay.js
+++ JavaScript/menuDisplay.js
@@ -6,8 +6,10 @@ const parsedUserData = JSON.parse(retrievedUserData);
 
 // Access data
 const user = parsedUserData.username;
+const userpin = parsedUserData.userpin;
 
-const userName = document.querySelector(".userName");
+const userName = document.querySelector(".userName h2");
 userName.innerText = user;
 
 console.log(user);
+console.log(userpin);
