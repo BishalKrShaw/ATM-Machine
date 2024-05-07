d3b3f4768807a4028eff36719cb998bdafb925e2
BishalKrShaw <bishalshaw.mfis@gmail.com>
2024-05-07 18:23:39 +0530

    Formatted done

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,21 +4,19 @@ const userPin = document.querySelector("#userPin");
 const submitBtn = document.querySelector("#submitBtn");
 
 const storeUserDetail = () => {
-    let username = userName.value;
-    let userbalance = userBalance.value;
-    let userpin = userPin.value;
+  let username = userName.value;
+  let userbalance = userBalance.value;
+  let userpin = userPin.value;
 
-    // Convert variables to JSON string
-    const userDataToStore = JSON.stringify(
-        {
-            username: username,
-            userbalance: userbalance,
-            userpin: userpin,
-        }
-    );
+  // Convert variables to JSON string
+  const userDataToStore = JSON.stringify({
+    username: username,
+    userbalance: userbalance,
+    userpin: userpin,
+  });
 
-    // Store JSON string in local storage
-    localStorage.setItem("userData", userDataToStore);
-}
+  // Store JSON string in local storage
+  localStorage.setItem("userData", userDataToStore);
+};
 
 submitBtn.addEventListener("click", storeUserDetail);
--- JavaScript/menuDisplay.js
+++ JavaScript/menuDisplay.js
@@ -11,7 +11,6 @@ const userpin = parsedUserData.userpin;
 const userName = document.querySelector(".userName h2");
 userName.innerText = user;
 
-
 // All options button
 const form = document.querySelector(".userName form");
 let dataSelectedOption = "";
@@ -19,35 +18,32 @@ let dataSelectedOption = "";
 const allOptionBtn = document.querySelectorAll("main a");
 
 allOptionBtn.forEach((optionBtn) => {
-    optionBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-        form.classList.add("block");
-        dataSelectedOption = optionBtn.dataset.option;
-    })
-})
-
+  optionBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    form.classList.add("block");
+    dataSelectedOption = optionBtn.dataset.option;
+  });
+});
 
 // Validating PIN
 const message = document.querySelector(".message");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Extracting value from input field
-    const pinValue = document.querySelector("#enterPin").value;
+  // Extracting value from input field
+  const pinValue = document.querySelector("#enterPin").value;
 
-    // Checking pinValue and userPin(localStorage)
-    if(pinValue === userpin) {
-        message.style.color = "white";
-        message.innerText = "Matched Successfully"
+  // Checking pinValue and userPin(localStorage)
+  if (pinValue === userpin) {
+    message.style.color = "white";
+    message.innerText = "Matched Successfully";
 
-        // Redirect to the next page after successful PIN Validation
-        window.location.href = `${dataSelectedOption}.html`;
-    }
-    else{
-            message.style.display = "block";
-            message.innerText = "Invalid Pin";
-            message.style.color = "red";
-    }
-
-})
+    // Redirect to the next page after successful PIN Validation
+    window.location.href = `${dataSelectedOption}.html`;
+  } else {
+    message.style.display = "block";
+    message.innerText = "Invalid Pin";
+    message.style.color = "red";
+  }
+});
